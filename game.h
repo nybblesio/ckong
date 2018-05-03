1e102b520cf8d576bdfb85ac3665c4082435e55c
Jeff Panici <jeff.panici@panici-software.com>
2018-05-03 12:06:34 -0600

    added comment header

--- game.h
+++ game.h
@@ -1,3 +1,15 @@
+// --------------------------------------------------------------------------
+//
+// C Kong
+// Copyright (C) 2018 Jeff Panici
+// All rights reserved.
+//
+// This software source file is licensed according to the
+// MIT License.  Refer to the LICENSE file distributed along
+// with this source file to learn more.
+//
+// --------------------------------------------------------------------------
+
 #pragma once
 
 #include <stdbool.h>
--- tile_map.c
+++ tile_map.c
@@ -1,3 +1,15 @@
+// --------------------------------------------------------------------------
+//
+// C Kong
+// Copyright (C) 2018 Jeff Panici
+// All rights reserved.
+//
+// This software source file is licensed according to the
+// MIT License.  Refer to the LICENSE file distributed along
+// with this source file to learn more.
+//
+// --------------------------------------------------------------------------
+
 #include "tile_map.h"
 
 static tile_map_t s_intro_tile_map = {
